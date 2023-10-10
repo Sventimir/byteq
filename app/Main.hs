29f7f83ba2cc849dc163514726c80ec9583a5834
Sventimir <svantimir@gmail.com>
2023-10-10 17:52:29 +0200

    Refactor the frontend and add dump command.

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 module Main (main) where
 
 import Control.Compiler (CompilationResult(..), compile)
@@ -20,6 +21,7 @@ main = do
 
 selectCmd :: String -> Maybe ([String] -> IO ())
 selectCmd "build" = Just build
+selectCmd "dump" = Just showInstr
 selectCmd "run" = Just run
 selectCmd "interp" = Just interp
 selectCmd _ = Nothing
@@ -29,42 +31,56 @@ interp args = do
   let program = case selectProgram (head args) of
                   Just p -> p
                   Nothing -> error "Unknown program"
-  case compile program of
-    Left err -> do
-      print err
-      exitWith (ExitFailure 1)
-    Right (CompilationResult _finStack instr) -> do
-      putStrLn $ dump instr
-      execute instr Empty
+  CompilationResult _finStack instr <- compileDAG program
+  execute instr Empty
 
 run :: [String] -> IO ()
 run args = do
-  bytes <- Bytes.readFile $ head args
-  case parseBytecode bytes of
-    Left err -> do
-      print err
-      exitWith (ExitFailure 1)
-    Right (BytecodeParseState bytes _finStack instr) -> do
-      when (Bytes.length bytes > 0) $ do
-        putStr "Trailing bytes in bytecode: "
-        print bytes
-      putStrLn $ dump instr
-      execute instr Empty
+  BytecodeParseState _bytes _finStack instr <- loadBytecode (head args)
+  execute instr Empty
 
 build :: [String] -> IO ()
 build args = do
   let program = case selectProgram (head args) of
                   Just p -> p
                   Nothing -> error "Unknown program"
-  case compile program of
-    Left err -> do
-      print err
-      exitWith (ExitFailure 1)
-    Right (CompilationResult _finStack instr) -> do
+  CompilationResult _finStack instr <- compileDAG program
+  writeBytecode (head args <> ".bc") instr
+
+showInstr :: [String] -> IO ()
+showInstr args = do
+  case selectProgram (head args) of
+    Just dag -> do
+      CompilationResult _finstack instr <- compileDAG dag
       putStrLn $ dump instr
-      writeBytecode (head args <> ".bc") instr
+    Nothing -> do
+      BytecodeParseState _ _ instr <- loadBytecode (head args)
+      putStrLn $ dump instr
+      
 
 selectProgram :: String -> Maybe (DAG.DAG ())
 selectProgram "example1" = Just DAG.example1
 selectProgram "fib" = Just DAG.exampleFib
 selectProgram _ = Nothing
+
+loadBytecode :: String -> IO (BytecodeParseState '[])
+loadBytecode path = do
+  bytes <- Bytes.readFile path
+  case parseBytecode bytes of
+    Left err -> do
+      print err
+      exitWith (ExitFailure 1)
+    Right r@(BytecodeParseState bs _finStack _instr) -> do
+      when (Bytes.length bs > 0) $ do
+        putStr "Trailing bytes in bytecode: "
+        print bs
+      return r
+
+compileDAG :: DAG.DAG () -> IO (CompilationResult '[])
+compileDAG dag = do
+  case compile dag of
+    Left err -> do
+      print err
+      exitWith (ExitFailure 1)
+    Right r@(CompilationResult _finStack _instr) -> do
+      return r 
